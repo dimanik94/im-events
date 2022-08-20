03852967f3d80e990e1395931a68362424eaa856
Аникин Дмитрий <d.anikin@infomaximum-llc.ru>
2022-08-20 15:12:33 +0300

    fix: fix ant style

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { FC } from "react";
+import { Calendar as AntCalendar } from "antd";
+
+const Calendar: FC = () => {
+  return <AntCalendar />;
+};
+
+export default Calendar;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,13 +2,16 @@
 import { FC } from "react";
 import { Content } from "antd/lib/layout/layout";
 import Sider from "antd/lib/layout/Sider";
+import Calendar from "../Calendar/Calendar";
 
 const Main: FC = () => {
   return (
     <>
-      <Sider css={{ borderRight: "1px solid gray" }}>Sider</Sider>
-      <Content style={{ width: "100%" }}>
-        <div css={{ color: "red" }}>Content</div>
+      <Sider theme="light" css={{ borderRight: "1px solid gray" }}>
+        Sider
+      </Sider>
+      <Content>
+        <Calendar />
       </Content>
     </>
   );
