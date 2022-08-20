a149ecbfe25a916dcd008ed18b126522ac8e2cd5
Александр Долганов <a.dolganov@infomaximum-llc.ru>
2022-08-20 15:15:11 +0300

    feat: добавил кнопку "войти"

--- src/components/Header/Header.styles.tsx
+++ src/components/Header/Header.styles.tsx
@@ -1,6 +1,14 @@
+// import LogoSvg from "../../resources/icons/Logo.svg";
+
 export const wrapperStyle = {
   display: "flex",
+  justifyContent: "space-between",
   backgroundColor: "#2c2c2c",
-  height: 48,
+  height: "48px !important",
   color: "white",
+  padding: "0 8px !important",
+};
+
+export const logoStyle = {
+  // backgroundImage: `url(${LogoSvg})`,
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,35 @@
 /** @jsxImportSource @emotion/react */
-import type { FC } from "react";
+import { FC } from "react";
 import type { IHeaderProps } from "./Header.types";
 import { Header as AntHeader } from "antd/lib/layout/layout";
-import { wrapperStyle } from "./Header.styles";
+import { logoStyle, wrapperStyle } from "./Header.styles";
+import { Avatar, Button } from "antd";
 
 const Header: FC<IHeaderProps> = () => {
-  return <AntHeader css={wrapperStyle}>Header</AntHeader>;
+  return (
+    <AntHeader css={wrapperStyle}>
+      <div css={logoStyle}>ЛОГО</div>
+      <div css={{ height: "48px", display: "flex" }}>
+        <Button
+          type="primary"
+          css={{ height: "28px", alignSelf: "center", marginRight: "8px" }}
+        >
+          Войти
+        </Button>
+        <Avatar
+          css={{
+            fontSize: "12px",
+            color: "#fff",
+            backgroundColor: "#FA8C16",
+            alignSelf: "center",
+          }}
+          size={20}
+        >
+          {"U"?.toUpperCase() ?? ""}
+        </Avatar>
+      </div>
+    </AntHeader>
+  );
 };
 
 export default Header;
